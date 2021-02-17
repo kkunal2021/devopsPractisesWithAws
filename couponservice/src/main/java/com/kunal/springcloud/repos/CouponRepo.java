c5f84b57bc4928c3ef8347035bf47466feec195f
Kumar Kunal <kkunal2005@gmail.com>
2021-02-17 20:52:57 +0400

    Adding couponservice Microservice # Initial

---
+++ couponservice/src/main/java/com/kunal/springcloud/repos/CouponRepo.java
@@ -0,0 +1,11 @@
+package com.kunal.springcloud.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kunal.springcloud.model.Coupon;
+
+public interface CouponRepo extends JpaRepository<Coupon, Long> {
+
+	Coupon findByCode(String code);
+
+}
